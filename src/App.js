2f4a2de2cc1e4d1c8b73d428d76257e8ddfd0f2d
SD-github21 <sapandonde.gmail.com>
2022-07-27 00:08:22 -0500

    added display of photos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React, { useState } from 'react';
-import About from './components/About';
 import Nav from './components/Nav';
+import About from './components/About';
 import Gallery from './components/Gallery';
 
 function App() {
   const [categories] = useState([
     {
-      name: "Commercial",
-      description: 
-         "Photos of grocery stores, food trucks, and other commercial projects",
-     },
-     { name: "Portraits", description: "Portraits of people in my life" },
-     { name: "Food", description: "Delicious delicacies" },
-     {
-      name: "Landscape",
-      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-     },
-  ]);
-  
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+  ]); 
+
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
   return (
@@ -26,12 +22,10 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
-       ></Nav>
+      ></Nav>
       <main>
-        <div>
-          <Gallery></Gallery>
-          <About></About>
-        </div>
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
       </main>
     </div>
   );
